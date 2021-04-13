9704b0efb4ef5ebd981911e89c049c11cfbb3630
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2021-04-13 01:39:01 -0500

    Enviando data del pais seleccionado

--- weather-app-front/src/components/CountryInfo/CountryInfo.js
+++ weather-app-front/src/components/CountryInfo/CountryInfo.js
@@ -7,7 +7,8 @@ import React from 'react';
 import './CountryInfo.scss';
 
 
-export default function CountryInfo(){
+export default function CountryInfo(props){
+    console.log(props);
     return(
         <h1>Informacion del clima de la ciudad-pais</h1>
     );
--- weather-app-front/src/pages/Search/Search.js
+++ weather-app-front/src/pages/Search/Search.js
@@ -1,28 +1,115 @@
 //Liberias
 import React,{ useState } from 'react';
-import {Layout} from 'antd';
+import {Layout,Form,Button,Input} from 'antd';
+import { CompassOutlined, GlobalOutlined, SearchOutlined  } from '@ant-design/icons';
+
 
 //Componentes
-import SearchBar from '../../components/SearchBar';
+//import SearchBar from '../../components/SearchBar';
 import CountryInfo from '../../components/CountryInfo';
+
+//Api
+import {getCurrentWeather} from '../../api/weather';
+
 //Estilos
 import './Search.scss';
 
 export default function Search(){
     const {Header,Content} = Layout;
+    const [results,setResults] = useState({
+        city:"",
+        country:"",
+        description:"",
+        humidity:"",
+        temperature:"",
+        wind: ""
+    })
+
+    const [inputs,setInputs] = useState({
+        city:"",
+        country: ""
+    });
+
+
+
+    const changeForm = e => {
+        setInputs({...inputs,[e.target.name]:e.target.value});
+    }
+
+    const resetForm = () => {
+        const inputs = document.getElementsByTagName('input');
+        inputs[0].value = "";
+      
+        setInputs({
+            city:"",
+            country:""
+        });
+    }
+
+    const search = async e => {
+        const result = await getCurrentWeather(inputs);
+        setResults({
+            city: result.name,
+            country:result.sys.name,
+            description:result.weather[0].description,
+            humidity:result.main.humidity,
+            temperature:result.main.temp,
+            wind: result.wind.speed
+        })
+        console.log(result);
+        resetForm();
+    }
    
 
     return(
         <Layout>
             <Layout className="layout-search">
                 <Header className="layout-search__header"> 
-                    <SearchBar/>
+                    <div className="search-bar">
+                        <Form className="search-bar__form" onFinish={search} onChange={changeForm}>
+                            <Form.Item>
+                                <Input className ="search-bar__input" 
+                                    size="large" 
+                                    placeholder="Ciudad" 
+                                    name="city"
+                                    prefix={<CompassOutlined />}
+                                    value={inputs.value}
+                                />
+                                </Form.Item>
+
+                                <Form.Item>
+                                <Input className ="search-bar__input" 
+                                    size="large" 
+                                    placeholder="País"
+                                    name="country"
+                                    prefix={<GlobalOutlined />}
+                                    value={inputs.value}
+                                />
+                                </Form.Item>
+
+                                <Form.Item>
+                                    <Button htmlType="submit" className="search-bar__button">
+                                        <SearchOutlined />
+                                    </Button>
+                            </Form.Item>
+
+                        </Form>
+                    </div>
                 </Header>
                 <Content className="layout-search__content">
-                    <CountryInfo/>
+                    <CountryInfo
+                        city={results.city}
+                        country={results.country}
+                        description={results.description}
+                        humidity={results.humidity}
+                        temperature={results.temperature}
+                        wind={results.wind}
+                    
+                    />
                 </Content>
             </Layout>
            
         </Layout>
     );
+
 }
